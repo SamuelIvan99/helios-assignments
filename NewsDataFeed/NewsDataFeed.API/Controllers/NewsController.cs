372ed4785839482226b09254156585ad07382f16
Samuel Ivan <1081504@ucn.dk>
2021-04-16 19:32:27 +0200

    Initial structure setup.

---
+++ NewsDataFeed/NewsDataFeed.API/Controllers/NewsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsDataFeed.Model;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewsDataFeed.API.Controllers
+{
+    [Produces("application/xml")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        // GET: api/<NewsController>
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://www.jao.eu/Web/News/MessageBoard/tso-rss.rss"))
+                {
+                    var result = await response.Content.ReadAsStreamAsync();
+                    var serializer = new XmlSerializer(typeof(Item));
+                    var items = serializer.Deserialize(result);
+                    return items;
+                }
+            }
+
+        }
+
+        // GET api/<NewsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NewsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NewsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NewsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NewsDataFeed/NewsDataFeed.Model/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NewsDataFeed.Model
+{
+    public class Item
+    {
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+
+        public string Guid { get; set; }
+
+        public DateTime PubDate { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+    }
+}
