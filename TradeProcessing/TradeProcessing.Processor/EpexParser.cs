ce55ad3d3e2f07c8e6bf1274f3f5487263f9241b
Samuel Ivan <1081504@ucn.dk>
2021-04-22 07:48:48 +0200

    Assignment 3 completed.

--- TradeProcessing/TradeProcessing.Processor/EpexParser.cs
+++ TradeProcessing/TradeProcessing.Processor/EpexParser.cs
@@ -34,59 +34,59 @@ namespace TradeProcessing.Processor
 
             return parser.ParseCsv(csv).ToList();
         }
+    }
 
-        public class EpexRowJao
-        {
-            [CsvColumn("Trader")]
-            public string TraderId { get; set; }
+    public class EpexRowJao
+    {
+        [CsvColumn("Trader")]
+        public string TraderId { get; set; }
 
-            [CsvColumn("TSO name")]
-            public string TSO { get; set; }
+        [CsvColumn("TSO name")]
+        public string TSO { get; set; }
 
-            [CsvColumn("EIC")]
-            public string EIC { get; set; }
+        [CsvColumn("EIC")]
+        public string EIC { get; set; }
 
-            [CsvColumn("B/S")]
-            public string BuySell { get; set; }
+        [CsvColumn("B/S")]
+        public string BuySell { get; set; }
 
-            [CsvColumn("Product")]
-            public string Product { get; set; }
+        [CsvColumn("Product")]
+        public string Product { get; set; }
 
-            [CsvColumn("Contract type")]
-            public string Contract { get; set; }
+        [CsvColumn("Contract type")]
+        public string Contract { get; set; }
 
-            [CsvColumn("Qantity")]
-            public decimal Quantity { get; set; }
+        [CsvColumn("Qantity")]
+        public decimal Quantity { get; set; }
 
-            [CsvColumn("Price")]
-            public decimal Price { get; set; }
+        [CsvColumn("Price")]
+        public decimal Price { get; set; }
 
-            [CsvColumn("Curr")]
-            public string Currency { get; set; }
+        [CsvColumn("Curr")]
+        public string Currency { get; set; }
 
-            [CsvColumn("Act")]
-            public string Act { get; set; }
+        [CsvColumn("Act")]
+        public string Act { get; set; }
 
-            [CsvColumn("Text")]
-            public string Text { get; set; }
+        [CsvColumn("Text")]
+        public string Text { get; set; }
 
-            [CsvColumn("State")]
-            public string State { get; set; }
+        [CsvColumn("State")]
+        public string State { get; set; }
 
-            [CsvColumn("Order No")]
-            public string OrderNo { get; set; }
+        [CsvColumn("Order No")]
+        public string OrderNo { get; set; }
 
-            [CsvColumn("Trade No")]
-            public string TradeNo { get; set; }
+        [CsvColumn("Trade No")]
+        public string TradeNo { get; set; }
 
-            [CsvColumn("P/O")]
-            public string P_O { get; set; }
+        [CsvColumn("P/O")]
+        public string P_O { get; set; }
 
-            [CsvColumn("Date/Time")]
-            public string DateTime { get; set; }
+        [CsvColumn("Date/Time")]
+        public string DateTime { get; set; }
 
-            [CsvColumn("BG")]
-            public string BG { get; set; }
-        }
+        [CsvColumn("BG")]
+        public string BG { get; set; }
     }
 }
